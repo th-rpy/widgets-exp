04422c05f41ce58cb17e8bf110940d2fed58adef
thamer_saraei <thamer.saraei@USherbrooke.ca>
2023-05-05 17:18:27 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-Please find below a more professional version of the provided text:
-
-- Create an Excel spreadsheet containing the required parameters to calculate alerts, and share it with Omar for completion with names and necessary information.
-- Utilize the completed Excel spreadsheet to configure alerts by adding and applying configurations and fixing parameter names as needed.
-- Begin integrating the parameters into the relevant function(s) that require them.
-- During the course of these activities, an additional task was undertaken where a discussion was held with AL regarding proposed changes. It was suggested that a dictionary be used instead of keyword arguments.
-- Implement the changes as discussed and test the updated code. Generate a CSV file containing all alerts for all equipment.
-- Commence a comparison of the alerts history with the newly generated alerts. For equipment 1, the same alerts were found for the months of April and March, spanning a two-month period.
