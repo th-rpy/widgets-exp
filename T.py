4c706f2db0bb9e674d645ea419c136c936b5448d
thamer_saraei <thamer.saraei@USherbrooke.ca>
2023-06-29 09:17:57 -0400

    Update T.py

--- T.py
+++ T.py
@@ -1,51 +1,58 @@
-# Create a list to store scatter traces for each signal
-traces = []
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
 
-# Iterate over the signals and create scatter traces
+# Create random binary signals for demonstration
+np.random.seed(0)
+signals = np.random.randint(0, 2, size=(100, 10))
+
+# Define the number of rows and columns for subplots
+num_rows = 5  # Number of rows of subplots
+num_cols = 2  # Number of columns of subplots
+
+# Create a list to store subplot figures
+subplot_figs = []
+
+# Iterate over the signals and create subplots
 for i in range(signals.shape[1]):
-    trace = go.Scatter(
-        x=np.arange(signals.shape[0]),
-        y=signals[:, i],
-        mode='markers',
-        name=f'Signal {i+1}'
+    # Create a new subplot figure
+    fig = go.Figure()
+
+    # Add a scatter trace for the current signal
+    fig.add_trace(
+        go.Scatter(
+            x=np.arange(signals.shape[0]),
+            y=signals[:, i],
+            mode='markers',
+            name=f'Signal {i+1}'
+        )
     )
-    traces.append(trace)
 
-# Create the layout
-layout = go.Layout(
-    title='Binary Signal Plots',
-    xaxis=dict(title='Time'),
-    yaxis=dict(title='Value'),
-    showlegend=True
-)
+    # Set the subplot layout
+    fig.update_layout(
+        title=f'Signal {i+1}',
+        xaxis=dict(title='Time'),
+        yaxis=dict(title='Value'),
+        showlegend=True
+    )
 
-# Create the figure with all scatter traces
-fig = go.Figure(data=traces, layout=layout)
+    # Append the subplot figure to the list
+    subplot_figs.append(fig)
 
-# Set the spacing between subplots to zero
-fig.update_layout(
-    margin=dict(t=40, r=40, b=40, l=40),
-    xaxis=dict(domain=[0, 1], anchor='y'),
-    yaxis=dict(domain=[0, 1], anchor='x'),
-    xaxis2=dict(domain=[0, 1], anchor='y2'),
-    yaxis2=dict(domain=[0, 1], anchor='x2'),
-    xaxis3=dict(domain=[0, 1], anchor='y3'),
-    yaxis3=dict(domain=[0, 1], anchor='x3'),
-    xaxis4=dict(domain=[0, 1], anchor='y4'),
-    yaxis4=dict(domain=[0, 1], anchor='x4'),
-    xaxis5=dict(domain=[0, 1], anchor='y5'),
-    yaxis5=dict(domain=[0, 1], anchor='x5'),
-    xaxis6=dict(domain=[0, 1], anchor='y6'),
-    yaxis6=dict(domain=[0, 1], anchor='x6'),
-    xaxis7=dict(domain=[0, 1], anchor='y7'),
-    yaxis7=dict(domain=[0, 1], anchor='x7'),
-    xaxis8=dict(domain=[0, 1], anchor='y8'),
-    yaxis8=dict(domain=[0, 1], anchor='x8'),
-    xaxis9=dict(domain=[0, 1], anchor='y9'),
-    yaxis9=dict(domain=[0, 1], anchor='x9'),
-    xaxis10=dict(domain=[0, 1], anchor='y10'),
-    yaxis10=dict(domain=[0, 1], anchor='x10')
-)
+# Create the merged plot
+fig = make_subplots(rows=num_rows, cols=num_cols, subplot_titles=[f'Signal {i+1}' for i in range(signals.shape[1])])
 
-# Display the figure
+# Assign each subplot figure to the corresponding subplot position and remove spacing
+for i in range(num_rows):
+    for j in range(num_cols):
+        fig.add_trace(subplot_figs[i * num_cols + j].data[0], row=i+1, col=j+1)
+        fig.update_layout(
+            {'xaxis' + str(i * num_cols + j + 1): {'anchor': 'y' + str(i * num_cols + j + 1)}},
+            {'yaxis' + str(i * num_cols + j + 1): {'anchor': 'x' + str(i * num_cols + j + 1)}}
+        )
+
+# Update the merged plot layout
+fig.update_layout(height=600, width=800, title_text='Binary Signal Plots', showlegend=False)
+
+# Display the merged plot
 fig.show()
